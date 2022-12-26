0bfc92992c0abd12ad77b5647b3e4e0af3089ad7
Shuke <stemirov777@gmal.com>
2022-12-26 17:50:25 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+"use strick";
+
+// // null
+// console.log(clientAge);
+
+// // undefined
+// let und;
+// console.log(und);
+
+// promt
+// const currentPlace = prompt("Where are you", "here");
+// console.log(currentPlace);
+
+// const favouriteColor = [];
+// favouriteColor[0] = prompt("What is your favourite color #1", "");
+// favouriteColor[1] = prompt("What is your favourite color #2", "");
+// favouriteColor[2] = prompt("What is your favourite color #3", "");
+// console.log(favouriteColor);
+
+// operatorlar
+
+// increment va decrement
+let incr = 10;
+let decr = 10;
+
+// incr++; -----> log(++incr)
+// decr--; -----> log(--decr)
+
+// console.log(incr);
+// console.log(decr);
+
+// prerics
+// console.log(incr++); bu yerda incrning qiymati bittaga oshdi lekin bu logda kurinmaydi. bundan keyin kurinadi
+// console.log(decr--); bu yerda incrning qiymati bittaga kamaydi lekin bu logda kurinmaydi. bundan keyin kurinadi
+
+// qoldiq (%)
+
+// let a = 10 % 3;
+// console.log(a); --> 1 chiqadi chunki 10ni 3ga bulsak 1 qoldiq qoladi
